b4dede6cad115dce00880415d284d77795c3199f
web77code <web77@yandex.ru>
2021-10-15 15:04:15 +0300

    Lot of small fixes from review

--- scripts/script.js
+++ scripts/script.js
@@ -13,20 +13,56 @@ function hideFormButtomHover () {
   formButtonElement.classList.remove('button_hover');
 }
 
+function addInputHighlighting () {
+  if(!formInputElement.classList.contains('order-form__email_status_empty')) {
+    formInputElement.classList.add('order-form__email_status_empty');
+  }
+}
+
+function removeInputHighlighting () {
+  if(formInputElement.classList.contains('order-form__email_status_empty')) {
+    formInputElement.classList.remove('order-form__email_status_empty');
+  }
+}
+
+function returnButtonTitle () {
+  formButtonElement.innerText = 'Записаться!';
+}
+
 headerMenuButton.addEventListener('click', () => {
-  headerMenu.classList.toggle('header__menu_opened');
-  headerMenuButton.classList.toggle('header__menu-button_close-icon');
+  if(headerMenuButton.classList.contains('header__menu-button_action_open')) {
+    headerMenuButton.classList.add('header__menu-button_action_close');
+    headerMenuButton.classList.remove('header__menu-button_action_open');
+    headerMenu.classList.toggle('header__menu_opened');
+  } else {
+    headerMenu.classList.toggle('header__menu_opened');
+    headerMenuButton.classList.remove('header__menu-button_action_close');
+    headerMenuButton.classList.add('header__menu-button_action_open');
+  }
 });
 
 formInputElement.addEventListener('focus', () => {
-  activeFormButtonHover ();
+  activeFormButtonHover();
+  removeInputHighlighting();
 });
 
 formInputElement.addEventListener('blur', () => {
-  hideFormButtomHover ();
+  hideFormButtomHover();
+});
+
+formInputElement.addEventListener('input', () => {
+  if(formInputElement.value.length === 0) {
+    returnButtonTitle();
+  }
 });
 
 formButtonElement.addEventListener('click', (evt) => {
   evt.preventDefault();
-  formButtonElement.innerText = 'Спасибо!';
+
+  if(formInputElement.value.length > 0) {
+    formButtonElement.innerText = 'Спасибо!';
+    formInputElement.value = '';
+  } else {
+    addInputHighlighting();
+  }
 });
